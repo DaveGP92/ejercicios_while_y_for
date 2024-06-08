2e728fc34fb7484cfd26e88f2802a9bd63798c14
Dave González Perdomo <dave.gonzalez1992@outlook.com>
2024-06-08 17:30:07 -0500

    Renombrando variables

--- 02_ejercicio_while.py
+++ 02_ejercicio_while.py
@@ -1,10 +1,10 @@
 # Escriba un programa que solicite una contraseña (el texto de la contraseña no es importante) y la vuelva a solicitar hasta que las dos contraseñas coincidan, con un límite de tres peticiones.
 
-contraseña = input("Ingrese su contraseña: \n")
-confirmacion = input("Confirme su contraseña: \n")
+password = input("Ingrese su contraseña: \n")
+confirmation = input("Confirme su contraseña: \n")
 intentos = 3
 
-while contraseña != confirmacion:
+while password != confirmation:
     print("Las contraseñas no coinciden.")
     intentos -= 1
     print(f"Le quedan {intentos} intentos.")
@@ -13,8 +13,8 @@ while contraseña != confirmacion:
         print("Cuenta bloqueada")
         break
     
-    contraseña = input("Ingrese de nuevo la contraseña:\n")
-    confirmacion = input("Confirme la contraseña:\n")
+    password = input("Ingrese de nuevo la contraseña:\n")
+    confirmation = input("Confirme la contraseña:\n")
 
 else:
     print("Las contraseñas coinciden.")
