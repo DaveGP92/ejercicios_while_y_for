5222f79b193e85f70389d61d7877c85d7f8b4120
Dave González Perdomo <dave.gonzalez1992@outlook.com>
2024-06-08 17:47:21 -0500

    Primer commit

---
+++ 03_ejercicio_while.py
@@ -0,0 +1,9 @@
+# Vamos a diseñar una calculadora que se enciende y hasta que no tecleamos 'SAL' no se apaga.
+
+# Esta calculadora funciona de la siguiente manera:
+
+# Recogemos los datos A y B
+# Si operación es 1 calcula la raíz cuadrada de la suma de A y B
+# Si operación es 2 calcula A / B. Vigilamos que B no sea 0...
+# Si la operación es 3 calculamos la siguiente fórmula: ( A * B ) / 2.5
+
