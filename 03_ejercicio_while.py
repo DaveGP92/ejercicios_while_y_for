01538312f4fd0338b8efd78674aa9b7f901c35d4
Dave González Perdomo <dave.gonzalez1992@outlook.com>
2024-06-08 18:28:15 -0500

    Creamos las variables para almacenar los números, y un diccionario con las opciones para escoger.

--- 03_ejercicio_while.py
+++ 03_ejercicio_while.py
@@ -7,3 +7,22 @@
 # Si operación es 2 calcula A / B. Vigilamos que B no sea 0...
 # Si la operación es 3 calculamos la siguiente fórmula: ( A * B ) / 2.5
 
+print("""    Bienvenido a mi calculadora. 
+    Escoge una de las siguiente opciones para operar: """)
+
+opcion = int(input("1: calcula la raíz cuadrada de la suma de A y B. '\n2: calcula A / B \n3: calculamos la siguiente fórmula: ( A * B ) / 2.5\n"))
+
+dict_opciones = {1 : "calcula la raíz cuadrada de la suma de A y B", 2 : "calcula A / B", 3 :"calculamos la siguiente fórmula: ( A * B ) / 2.5" }
+
+print(f"Escogiste la opción {opcion}: {dict_opciones[opcion]}")
+
+print("Para salir escribe 'SAL'")
+
+
+print("Ahora vamos a ingresar los números para operar: ")
+num1 = int(input("Ingresa el número A:\n"))
+
+num2 = int(input("Ingresa el número B:\n"))
+
+
+
