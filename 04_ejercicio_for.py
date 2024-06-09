03bf0953ea1f1785d8aead34433ecef83c22b74c
Dave González Perdomo <dave.gonzalez1992@outlook.com>
2024-06-09 14:41:14 -0500

    Agregando ciclo for para recorrer la frase ingresada por el usuario.

--- 04_ejercicio_for.py
+++ 04_ejercicio_for.py
@@ -1,9 +1,11 @@
 print("Escribir un programa en el que se pregunte al usuario por una frase y una letra, y muestre por pantalla el número de veces que aparece la letra en la frase.")
 
-frase = input("Escriba una frase:\n")
+frase = input("Escriba una frase:\n").lower()
 
-letra_escogida = input("Escoge una letra del alfabeto:\n")
+letra_escogida = input("Escoge una letra del alfabeto:\n").lower()
 
-# for letra in frase:
-#     if letra_escogida == letra:
-#         print(letra.count(letra_escogida))
+for letra in frase:
+    if letra_escogida == letra:
+        contador_letra = frase.count(letra_escogida)
+
+print(f'La letra "{letra_escogida}" aparece {contador_letra} veces dentro de la frase o palabra "{frase}"')
