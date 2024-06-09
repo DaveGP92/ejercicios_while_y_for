479f552ebef0174e2faffc697ef4c6ddf9fd52fe
Dave González Perdomo <dave.gonzalez1992@outlook.com>
2024-06-09 14:53:09 -0500

    Primer commit. Creando variables.

---
+++ 05_ejercicio_for.py
@@ -0,0 +1,3 @@
+print("Escribir un programa que pida al usuario una palabra y luego muestre por pantalla una a una las letras de la palabra introducida empezando por la última.")
+
+palabra_selecc = input("Por favor, introduzca una palabra:\n")
