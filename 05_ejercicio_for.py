36951fb4383b09e54f6c087bd841aabc62ba7dec
Dave González Perdomo <dave.gonzalez1992@outlook.com>
2024-06-09 15:31:58 -0500

    Creando e ciclo for para recorrer la palabra.

--- 05_ejercicio_for.py
+++ 05_ejercicio_for.py
@@ -1,3 +1,10 @@
 print("Escribir un programa que pida al usuario una palabra y luego muestre por pantalla una a una las letras de la palabra introducida empezando por la última.")
 
 palabra_selecc = input("Por favor, introduzca una palabra:\n")
+palabra_reves = []
+
+for letra in palabra_selecc:
+    palabra_reves.append(letra)
+
+palabra_reves.reverse()
+print(palabra_reves)
