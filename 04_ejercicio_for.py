f58e1c9f30c3c6f8690112b8551d3846c8442f43
Dave González Perdomo <dave.gonzalez1992@outlook.com>
2024-06-09 14:22:29 -0500

    Primer commit. Creando variables

---
+++ 04_ejercicio_for.py
@@ -0,0 +1,9 @@
+print("Escribir un programa en el que se pregunte al usuario por una frase y una letra, y muestre por pantalla el número de veces que aparece la letra en la frase.")
+
+frase = input("Escriba una frase:\n")
+
+letra_escogida = input("Escoge una letra del alfabeto:\n")
+
+# for letra in frase:
+#     if letra_escogida == letra:
+#         print(letra.count(letra_escogida))
